2b7223de3b3d13edbcf24584a6841a2973a0630f
Mateus5555577777 <mateusabreu@souunilavras.com>
2024-09-30 19:10:09 -0300

    Atualizado pelo Mateus

--- src/components/EditTask.jsx
+++ src/components/EditTask.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import './EditTask.css'; // Importa o arquivo CSS
+import './EditTask.css'; 
 
 function EditTask() {
   const { id } = useParams();
